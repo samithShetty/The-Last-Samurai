fd7623fc93d62409f64ba1670cfb05ca3568b08f
Samith Shetty <71335825+samithShetty@users.noreply.github.com>
2023-08-02 16:20:52 -0400

    Set up Unreal Project + VS Code

---
+++ README.md
@@ -0,0 +1,3 @@
+# TheLastSamurai
+
+Developed with Unreal Engine 5
---
+++ Source/TheLastSamurai/TheLastSamuraiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheLastSamuraiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THELASTSAMURAI_API ATheLastSamuraiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
